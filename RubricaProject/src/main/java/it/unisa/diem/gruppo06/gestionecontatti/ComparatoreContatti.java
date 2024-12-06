c731f7318e80d29ceda494c4264a49ff2f127a40
Ema <Ema@DESKTOP-0JO8O03>
2024-12-06 15:02:59 +0100

    Third commit

--- RubricaProject/src/main/java/it/unisa/diem/gruppo06/gestionecontatti/ComparatoreContatti.java
+++ RubricaProject/src/main/java/it/unisa/diem/gruppo06/gestionecontatti/ComparatoreContatti.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package it.unisa.diem.gruppo06.gestionecontatti;
 
 import java.util.Comparator;
 
-/**
- *
- * @author grego
- */
 public class ComparatoreContatti implements Comparator<Contatto>{
 
     @Override
