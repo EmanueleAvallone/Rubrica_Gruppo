bf933f9940f3fe73a92efce50a5d6d9239633bac
Ema <Ema@DESKTOP-0JO8O03>
2024-12-06 15:00:54 +0100

    Second Commit

--- RubricaProject/src/main/java/it/unisa/diem/gruppo06/gestionecontatti/ComparatoreContatti.java
+++ RubricaProject/src/main/java/it/unisa/diem/gruppo06/gestionecontatti/ComparatoreContatti.java
@@ -14,10 +14,7 @@ public class ComparatoreContatti implements Comparator<Contatto>{
 
     @Override
     public int compare(Contatto o1, Contatto o2) {
-        if(o1.getCognome().equals(o2.getCognome()))
-            return o1.getNome().compareTo(o2.getNome());
-        
-        return o1.getCognome().compareTo(o2.getCognome());
+        return 0;
     }
     
 }
