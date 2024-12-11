51bdafaa182587b2f5d82bbfeeba5b10c9ae54bc
Ema <Ema@DESKTOP-0JO8O03>
2024-12-11 16:02:49 +0100

    Test ComparatoreContatti

--- RubricaProject/src/test/java/it/unisa/diem/gruppo06/gestionecontatti/ComparatoreContattiTest.java
+++ RubricaProject/src/test/java/it/unisa/diem/gruppo06/gestionecontatti/ComparatoreContattiTest.java
@@ -1,10 +1,8 @@
-
 package it.unisa.diem.gruppo06.gestionecontatti;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 public class ComparatoreContattiTest {
     private Contatto o1;
@@ -15,13 +13,15 @@ public class ComparatoreContattiTest {
        
     @BeforeEach
     public void setUp() {
-        
-      
         instance = new ComparatoreContatti();
     }
+    
     /**
      * Test of compare method, of class ComparatoreContatti.
-     */
+     * 
+     * Contatti con nome e cognome uguali
+     */ 
+    
     @Test
     public void testCompare1() { 
         String[] numeri = { "3352637284","3443365728", null};
@@ -34,6 +34,13 @@ public class ComparatoreContattiTest {
         int result = instance.compare(o1, o2);
         assertEquals(expResult, result);      
     }
+    
+    /**
+     * Test of compare method, of class ComparatoreContatti.
+     * 
+     * * Contatti con cognome diverso
+     */ 
+    
     @Test
     public void testCompare2() {
         String[] numeri = { "3352637284","3443365728", null};
@@ -47,17 +54,58 @@ public class ComparatoreContattiTest {
         
     }
     
+    /**
+     * Test of compare method, of class ComparatoreContatti.
+     * 
+     * * Contatti con cognome diverso
+     */ 
+    
     @Test
     public void testCompare3() { 
         String[] numeri = { "3352637284","3443365728", null};
         String[] email = {"e1@gmail.com","e2@gmail.com", null};
-        
         o1 = new Contatto("Emanuele","Barberio",numeri,email);
         o2 = new Contatto("Emanuele","Avallone",numeri,email); 
         int expResult = 1;
         int result = instance.compare(o1, o2);
         assertEquals(expResult, result);
-        
     }
     
+    /**
+     * Test of compare method, of class ComparatoreContatti.
+     * 
+     * * Contatti con nome diverso
+     */ 
+    
+    @Test
+    public void testCompare4() { 
+        String[] numeri = { "3352637284","3443365728", null};
+        String[] email = {"e1@gmail.com","e2@gmail.com", null};
+        
+        o1 = new Contatto("Gregorio","Avallone",numeri,email);
+        o2 = new Contatto("Emanuele","Avallone",numeri,email); 
+        int expResult = 2;
+        instance = new ComparatoreContatti();
+        int result = instance.compare(o1, o2);
+        assertEquals(expResult, result);      
+    }
+    
+    /**
+     * Test of compare method, of class ComparatoreContatti.
+     * 
+     * Contatti con nome diverso
+     */ 
+    
+    @Test
+    public void testCompare5() { 
+        String[] numeri = { "3352637284","3443365728", null};
+        String[] email = {"e1@gmail.com","e2@gmail.com", null};
+        
+        o1 = new Contatto("Emanuele","Avallone",numeri,email);
+        o2 = new Contatto("Gregorio","Avallone",numeri,email); 
+        int expResult = -2;
+        instance = new ComparatoreContatti();
+        int result = instance.compare(o1, o2);
+        assertEquals(expResult, result);      
+    }
 }
